27a6bb23453e03b820e8b0aff3476c842caf4e42
jean <bayihajean1991@gmail.com>
2025-12-18 04:57:13 +0000

    Suppression des fichiers vides

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPEN_AI_API_KEY = os.getenv("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")
-PASSWORD = os.getenv("PASSWORD", "boutik123") #default password
